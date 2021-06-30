5022a83d1b77166b09da60cc0d1dd91fde4d9e47
bamdadam <kanra252@gmail.com>
2021-06-30 05:52:26 +0430

    changed Financial model and added Company

--- core/models.py
+++ core/models.py
@@ -2,19 +2,24 @@ from django.db import models
 
 
 # Create your models here.
+class Company(models.Model):
+    CompanyName = models.CharField(max_length=30)
+
+
 # simple Financial model based of Financial data highlighted from PBGC's financial statements
 # for the periods ending September 30, 1992 through September 30, 2009.
 # https://data.world/finance/pbgc-financial-summary-data
 # at the moment we dont have any particular data dependency between tables
 class FinancialSummary(models.Model):
-    PremiumIncome = models.IntegerField()
-    OtherIncome = models.IntegerField()
-    InvestmentIncome = models.IntegerField()
-    Credits = models.IntegerField()
-    Expenses = models.IntegerField()
     NetIncome = models.IntegerField()
-    Year = models.PositiveIntegerField(unique=True)
+    Year = models.PositiveIntegerField()
+    Company = models.ForeignKey(Company, related_name='FinancialSummary', on_delete=models.CASCADE,)
 
+    class Meta:
+        unique_together = ['Company', 'Year']
+
+    def __str__(self):
+        return 'ok'
 
 # class FinancialPosition(models.Model):
 #     CashAndInvestments = models.IntegerField()
--- core/serializers.py
+++ core/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from core.models import FinancialSummary
 
+
 # simple Serializer class for FinancialSummary model
 # for now it takes all the fields and just Serializes them
 class FinancialSummarySerializer(serializers.ModelSerializer):
     class Meta:
         model = FinancialSummary
-        fields = ('PremiumIncome', 'OtherIncome', 'InvestmentIncome',
-                  'Credits', 'Expenses', 'NetIncome', 'Year', 'id')
+        fields = '__all__'

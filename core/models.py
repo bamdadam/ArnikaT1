ed4c0800fab1130043648bbcacbe7110ee23f5f3
bamdadam <kanra252@gmail.com>
2021-07-04 18:33:45 +0430

    next commit

--- core/models.py
+++ core/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Company(models.Model):
     CompanyName = models.CharField(max_length=30)
 
+    def __str__(self):
+        return f'{self.id} : {self.CompanyName} : {self.financialsummary}'
+
 
 # simple Financial model based of Financial data highlighted from PBGC's financial statements
 # for the periods ending September 30, 1992 through September 30, 2009.
@@ -12,17 +15,12 @@ class Company(models.Model):
 # at the moment we dont have any particular data dependency between tables
 class FinancialSummary(models.Model):
     NetIncome = models.IntegerField(default=0)
-    Year = models.PositiveIntegerField()
+    Year = models.DateField()
     Company = models.ForeignKey(Company, related_name='financialsummary', on_delete=models.CASCADE,)
 
     class Meta:
         unique_together = ['Company', 'Year']
 
     def __str__(self):
-        return f"{self.Company  .CompanyName}:{self.Year}:{self.NetIncome}"
+        return f"{self.Company.CompanyName}:{self.Year}:{self.NetIncome}"
 
-# class FinancialPosition(models.Model):
-#     CashAndInvestments = models.IntegerField()
-#     TotalAssets = models.IntegerField()
-#     NetPosition = models.IntegerField()
-#     FinancialSummary = models.OneToOneField(FinancialSummary, on_delete=models.CASCADE)
--- core/serializers.py
+++ core/serializers.py
@@ -1,3 +1,6 @@
+import traceback
+from datetime import datetime, date
+
 from rest_framework import serializers
 from core.models import FinancialSummary, Company
 
@@ -5,23 +8,42 @@ from core.models import FinancialSummary, Company
 # simple Serializer class for FinancialSummary model
 # for now it takes all the fields and just Serializes them
 class FinancialSummarySerializer(serializers.ModelSerializer):
+    # print(validated_data)
+
     class Meta:
         model = FinancialSummary
         fields = ['NetIncome', 'Year']
 
+    def validate_Year(self, value):
+        try:
+            # print(value)
+            # date = datetime.strptime(value, '%Y-%m-%d')
+            if value > datetime.now().date():
+                raise serializers.ValidationError("Please enter a valid Date")
+        except ValueError:
+            raise serializers.ValidationError("Incorrect data format, should be YYYY-MM-DD")
+
 
 class CompanySerializer(serializers.ModelSerializer):
-    financialsummary = FinancialSummarySerializer(many=True,)
+    financialsummary = FinancialSummarySerializer(many=True, )
 
     class Meta:
         model = Company
-        fields = ['CompanyName', 'financialsummary']
+        fields = ['CompanyName', 'financialsummary', 'id']
 
     def create(self, validated_data):
-        print(validated_data)
         finances_data = validated_data.pop('financialsummary')
-        company = Company.objects.create(**validated_data)
-        for finance_data in finances_data:
-            FinancialSummary.objects.create(Company=company, **finance_data)
-
+        try:
+            company = Company.objects.create(**validated_data)
+            for finance_data in finances_data:
+                try:
+                    print(finance_data)
+                    FinancialSummary.objects.create(Company=company, **finance_data)
+                except TypeError:
+                    tb = traceback.format_exc()
+                    raise TypeError(tb)
+        except TypeError:
+            tb = traceback.format_exc()
+            # print(tb)
+            raise TypeError(tb)
         return company

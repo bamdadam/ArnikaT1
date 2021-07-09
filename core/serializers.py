bd0ab22e2bec97255650fd6e7187ec90067a7556
bamdadam <kanra252@gmail.com>
2021-07-09 19:25:45 +0430

    Finished Company Creation  and list with Respect to FinancialSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,5 @@
 import traceback
-from typing import Dict
-from datetime import datetime, date
-
+from datetime import datetime
 from rest_framework import serializers
 from core.models import FinancialSummary, Company
 
@@ -10,45 +8,35 @@ from core.models import FinancialSummary, Company
 # for now it takes all the fields and just Serializes them
 
 class FinancialSummarySerializer(serializers.ModelSerializer):
-    # print(validated_data)
-
     class Meta:
         model = FinancialSummary
-        fields = ['NetIncome', 'Year']
+        fields = ['NetIncome', 'Year', 'Company']
 
-    # def validate_Year(self, value):
-    #     try:
-    #         # print(value)
-    #         # date = datetime.strptime(value, '%Y-%m-%d')
-    #         if value > datetime.now().date():
-    #             raise serializers.ValidationError("Please enter a valid Date")
-    #     except ValueError:
-    #         raise serializers.ValidationError("Incorrect data format, should be YYYY-MM-DD")
+    def validate_Year(self, value):
+        try:
+            # print(value)
+            # date = datetime.strptime(value, '%Y-%m-%d')
+            if value > datetime.now().date():
+                raise serializers.ValidationError("Please enter a valid Date", code='Invalid Date')
+            return value
+        except ValueError:
+            raise serializers.ValidationError("Incorrect data format, should be YYYY-MM-DD")
+
+    def to_internal_value(self, data):
+        print(data)
 
 
 class CompanySerializer(serializers.ModelSerializer):
-    financialsummary = FinancialSummarySerializer(many=True, required=False)
+    financialsummary = FinancialSummarySerializer(required=False, many=True)
 
     class Meta:
         model = Company
         fields = ['CompanyName', 'financialsummary', 'id']
 
     def create(self, validated_data):
-        if "financialsummary" in validated_data:
-            finances_data = validated_data.pop('financialsummary')
-        else:
-            finances_data = {}
         try:
             company = Company.objects.create(**validated_data)
-            for finance_data in finances_data:
-                try:
-                    print(finance_data)
-                    FinancialSummary.objects.create(Company=company, **finance_data)
-                except TypeError:
-                    tb = traceback.format_exc()
-                    raise TypeError(tb)
         except TypeError:
             tb = traceback.format_exc()
-            # print(tb)
             raise TypeError(tb)
         return company

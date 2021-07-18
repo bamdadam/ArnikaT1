4e809cb13808fc28d155e802eafa32be165482b9
bamdadam <kanra252@gmail.com>
2021-07-18 12:46:45 +0430

    fixed url lowercase

--- core/urls.py
+++ core/urls.py
@@ -6,10 +6,10 @@ from core.views import FinancialSummaryListCreateAPIView, \
 # urls for implemented views
 app_name = 'core_app'
 urlpatterns = [
-    path('FinancialSummary/', FinancialSummaryListCreateAPIView.as_view(),),
-    path('FinancialSummary/<int:Company>/<str:Year>/', FinancialSummaryRetrieveUpdateDestroyAPIView.as_view()),
-    path('FinancialSummary/<int:NetIncome>/', FinancialSummaryListCreateAPIView.as_view()),
-    path('Company/', CompanyListCreateAPIView.as_view(), ),
-    path('Company/<int:pk>/', CompanyRetrieveUpdateDeleteAPIView.as_view(),),
+    path('financialsummary/', FinancialSummaryListCreateAPIView.as_view(),),
+    path('financialsummary/<int:Company>/<str:Year>/', FinancialSummaryRetrieveUpdateDestroyAPIView.as_view()),
+    path('financialsummary/<int:NetIncome>/', FinancialSummaryListCreateAPIView.as_view()),
+    path('company/', CompanyListCreateAPIView.as_view(), ),
+    path('company/<int:pk>/', CompanyRetrieveUpdateDeleteAPIView.as_view(),),
 ]
 

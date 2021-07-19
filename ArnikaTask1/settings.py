013cf39b15ecca28847314acbc44a43a5c4d853c
bamdadam <kanra252@gmail.com>
2021-07-19 12:40:38 +0430

    implemented url converter for persian dates.

--- ArnikaTask1/settings.py
+++ ArnikaTask1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!_+dp!hqbsoxca64e$ewy#2r4mph4--7dg9xj3di!%ixt4zy+z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
